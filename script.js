a2b283c98f8e9d595ef8ab608013e924327094af
Navroz-Singh <navrozsingh9@gmail.com>
2024-07-18 22:58:05 +0530

    updated js file

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ let ip_toSearch = document.getElementById("ip_toSearch")
 let search_btn = document.body.getElementsByClassName("search_btn")[0]
 let entered_ip
 
-
 let loc_coordinates
 let ip_info_IP
 let ip_info_city
@@ -16,86 +15,23 @@ let ip_object
 
 let UTCoffset = "UTC"
 
-
 //IP info 
 let ip_url = "https://ipinfo.io?token=9c1a4f4eaef5ea"
 
-async function get_ip_info(){
-    let response = await fetch(ip_url)
-    return response.json()  
-}
-get_ip_info().then( data=>
-    localStorage.setItem("ip_object", JSON.stringify(data))
-)
-ip_object = JSON.parse(localStorage.getItem("ip_object")) //strored the data obtained in ip_object
-ip_info_IP = ip_object.ip
-ip_info_city = ip_object.city
-ip_info_state = ip_object.region
-ip_info_postal = ip_object.postal
-ip_info_timezone = ip_object.timezone
-loc_coordinates = ip_object.loc
+// Initialize map variable
+let map;
+let MapMarker;
 
-let latitude = Number.parseFloat(loc_coordinates.slice(0, loc_coordinates.indexOf(",")))
-let longitude = Number.parseFloat(loc_coordinates.slice(loc_coordinates.indexOf(",")+1, loc_coordinates.length))
-
-
-
-//-------------------------MAP---------------------------------
-let map = L.map("map").setView([`${latitude}`,`${longitude}`], 19)
-
-let MapMarker = L.icon({
-    iconUrl : "images/icon-location.svg",
-    iconSize:[46, 56]
-})
-
-L.marker([`${latitude}`,`${longitude}`], {icon: MapMarker}).addTo(map)
-
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
-
-
-
-let worldtimeapi = `https://worldtimeapi.org/api/timezone/${ip_info_timezone}`
-async function getworldtimeapi(){
-    let response = await fetch(worldtimeapi);
+async function get_ip_info(url) {
+    let response = await fetch(url)
     return response.json()
 }
 
-getworldtimeapi().then(data=>{
-    localStorage.setItem("utc_offset", JSON.stringify(data))
-})
+async function initialize() {
+    let data = await get_ip_info(ip_url)
+    localStorage.setItem("ip_object", JSON.stringify(data))
+    ip_object = data
 
-UTCoffset += JSON.parse(localStorage.getItem("utc_offset")).utc_offset
-
-
-//Displaying User's information upon page load
-{
-
-    ip_info.innerHTML = `${ip_info_IP}`
-    location_info.innerHTML = `${ip_info_city} ${ip_info_state} ${ip_info_postal}`
-    timezone_info.innerHTML = `${UTCoffset}`
-    
-}
-    
-ip_toSearch.addEventListener("keyup", function(){
-    entered_ip = this.value
-})
-
-search_btn.addEventListener("click", function(){
-
-    ip_url = `https://ipinfo.io/${entered_ip}?token=9c1a4f4eaef5ea`
-    async function get_ip_info(){
-        let response = await fetch(ip_url)
-        return response.json()  
-    }
-    get_ip_info().then( function(data){
-        localStorage.setItem("ip_object2", JSON.stringify(data))
-        console.log(data)
-    }
-    )
-    ip_object = JSON.parse(localStorage.getItem("ip_object2")) //strored the data obtained in ip_object
     ip_info_IP = ip_object.ip
     ip_info_city = ip_object.city
     ip_info_state = ip_object.region
@@ -104,31 +40,71 @@ search_btn.addEventListener("click", function(){
     loc_coordinates = ip_object.loc
 
     let latitude = Number.parseFloat(loc_coordinates.slice(0, loc_coordinates.indexOf(",")))
-    let longitude = Number.parseFloat(loc_coordinates.slice(loc_coordinates.indexOf(",")+1, loc_coordinates.length))
+    let longitude = Number.parseFloat(loc_coordinates.slice(loc_coordinates.indexOf(",") + 1, loc_coordinates.length))
 
+    // Initialize map
+    map = L.map("map").setView([latitude, longitude], 19)
 
-    let worldtimeapi = `https://worldtimeapi.org/api/timezone/${ip_info_timezone}`
-    async function getworldtimeapi(){
-        let response = await fetch(worldtimeapi);
-        return response.json()
-    }
-
-    getworldtimeapi().then(data=>{
-        localStorage.setItem("utc_offset2", JSON.stringify(data))
+    MapMarker = L.icon({
+        iconUrl: "images/icon-location.svg",
+        iconSize: [46, 56]
     })
 
-    UTCoffset = "UTC"
+    L.marker([latitude, longitude], { icon: MapMarker }).addTo(map)
 
-    UTCoffset += JSON.parse(localStorage.getItem("utc_offset2")).utc_offset
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map)
+
+    let worldtimeapi = `https://worldtimeapi.org/api/timezone/${ip_info_timezone}`
+    let timeData = await getworldtimeapi(worldtimeapi)
+    localStorage.setItem("utc_offset", JSON.stringify(timeData))
+    UTCoffset += JSON.parse(localStorage.getItem("utc_offset")).utc_offset
+
+    // Displaying User's information upon page load
+    ip_info.innerHTML = `${ip_info_IP}`
+    location_info.innerHTML = `${ip_info_city} ${ip_info_state} ${ip_info_postal}`
+    timezone_info.innerHTML = `${UTCoffset}`
+}
+
+async function getworldtimeapi(url) {
+    let response = await fetch(url);
+    return response.json();
+}
+
+search_btn.addEventListener("click", async function () {
+    entered_ip = ip_toSearch.value
+    ip_url = `https://ipinfo.io/${entered_ip}?token=9c1a4f4eaef5ea`
+
+    let data = await get_ip_info(ip_url)
+    localStorage.setItem("ip_object2", JSON.stringify(data))
+    ip_object = data
+
+    ip_info_IP = ip_object.ip
+    ip_info_city = ip_object.city
+    ip_info_state = ip_object.region
+    ip_info_postal = ip_object.postal
+    ip_info_timezone = ip_object.timezone
+    loc_coordinates = ip_object.loc
+
+    let latitude = Number.parseFloat(loc_coordinates.slice(0, loc_coordinates.indexOf(",")))
+    let longitude = Number.parseFloat(loc_coordinates.slice(loc_coordinates.indexOf(",") + 1, loc_coordinates.length))
+
+    let worldtimeapi = `https://worldtimeapi.org/api/timezone/${ip_info_timezone}`
+    let timeData = await getworldtimeapi(worldtimeapi)
+    localStorage.setItem("utc_offset2", JSON.stringify(timeData))
+
+    UTCoffset = "UTC" + JSON.parse(localStorage.getItem("utc_offset2")).utc_offset
 
     ip_info.innerHTML = `${ip_info_IP}`
     location_info.innerHTML = `${ip_info_city} ${ip_info_state} ${ip_info_postal}`
     timezone_info.innerHTML = `${UTCoffset}`
 
-
-
-
-    map.setView([latitude, longitude], 19);
-    L.marker([`${latitude}`,`${longitude}`], {icon: MapMarker}).addTo(map)
-
+    // Update map
+    map.setView([latitude, longitude], 19)
+    L.marker([latitude, longitude], { icon: MapMarker }).addTo(map)
 })
+
+// Initialize on page load
+initialize()
